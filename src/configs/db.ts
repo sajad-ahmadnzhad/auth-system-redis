9a1c58ffda8683e74d5bdad64de2e63ad58fee62
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-03-22 01:41:51 +0430

    feat: deploy project in liara

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const client = new Redis();
+const client = new Redis(process.env.REDIS_URL as string);
 
 client.on("connect", () => {
   console.log("Connected to redis successfully");
@@ -10,4 +10,4 @@ client.on("error", (error) => {
   console.log("Connection redis error", error);
 });
 
-export default client
+export default client;
